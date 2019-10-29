bee6300aa0ba4bc362f50bb94af986fe4b1f08b7
RaghadHS <raghad.alsawalhi@gmail.com>
2019-10-29 17:39:49 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"user strict";
+
+var = x ;
+var = a;
+
+//ask user for their name
+x= window.prompt('Hello, what is your name?');
+
+document,write('Welcome,' + x );
+
+//ask the user for their age
+a= window.prompt('How old are you?');
+
+for (i=0; i < a; i++)
+{
+    
+    document.write('@' + '<br>');
+    
+    {
